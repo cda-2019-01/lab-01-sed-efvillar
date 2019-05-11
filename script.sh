719f7586475966e2b88b811cf0f1550e54843046
Edwin Fernando Villarraga Ossa <efvillar@unal.edu.co>
2019-05-11 08:40:57 -0500

    Final Commit

--- script.sh
+++ script.sh
@@ -4,9 +4,6 @@ sed 's/;;/;\\N;/' deta2.csv > deta3.csv
 sed 's/\([0-9][0-9][0-9]\);\([0-9]\)/\1,\2/' deta3.csv > deta4.csv
 sed 's/\([0-9][0-9][0-9]\),\([0-9]\)/\1.\2/' deta4.csv > deta5.csv
 sed 's/;/,/g' deta5.csv > deta6.csv
-#sed 's/,$/,\\N/g' deta6.csv > deta7.csv
-sed 's/,/,\\N/3g' deta6.csv > deta7.csv
-sed 's/\\N\\N/\\N/' deta7.csv > deta8.csv
-sed 's/\\N\([0-9]\)/\1/' deta8.csv > deta9.csv
-sed '/\\N/d' deta9.csv > data_final.csv
+sed 's/,\r/,\\N/g' deta6.csv > deta7.csv
+sed '/\\N/d' deta7.csv > data_final.csv
 rm deta*
